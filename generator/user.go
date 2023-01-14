376d2640f7f93554e151bd30875d2b176f447c91
Laba Subagia <labasubagia22@gmail.com>
2023-01-14 22:02:19 +0800

    use pointer example

--- generator/user.go
+++ generator/user.go
@@ -2,13 +2,15 @@ package generator
 
 import (
 	"fmt"
+	"time"
 )
 
 type User struct {
-	ID      int    `db:"id"`
-	Name    string `db:"name"`
-	Age     int    `db:"age"`
-	Address string `db:"address"`
+	ID        *int       `db:"id"`
+	Name      *string    `db:"name"`
+	Age       *int       `db:"age"`
+	Address   *string    `db:"address"`
+	UpdatedAt *time.Time `db:"updated_at"`
 }
 
 func NewUserDump() GenerateDump[User] {
@@ -24,19 +26,25 @@ func (u *User) Current() User {
 }
 
 func (u *User) DumpCreate(no int) User {
+	name := fmt.Sprintf("Create Name %d", no)
+	address := fmt.Sprintf("Create Address %d", no)
 	return User{
-		ID:      no,
-		Age:     no,
-		Name:    fmt.Sprintf("Create Name %d", no),
-		Address: fmt.Sprintf("Create Address %d", no),
+		ID:      &no,
+		Age:     &no,
+		Name:    &name,
+		Address: &address,
 	}
 }
 
 func (u *User) DumpUpdate(no int) User {
+	name := fmt.Sprintf("Edited Name %d", no)
+	address := fmt.Sprintf("Edited Address %d", no)
+	now := time.Now()
 	return User{
-		ID:      no,
-		Age:     no,
-		Name:    fmt.Sprintf("Update Name %d", no),
-		Address: fmt.Sprintf("Update Address %d", no),
+		ID:        &no,
+		Age:       &no,
+		Name:      &name,
+		Address:   &address,
+		UpdatedAt: &now,
 	}
 }
